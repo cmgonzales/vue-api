499c194f2d9b8d7cf459f9c1a01e7640ee136abd
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-03-31 18:18:57 -0700

    added props

---
+++ src/components/right.vue
@@ -0,0 +1,14 @@
+<template>
+<app-weather>{{info}}</app-weather>
+ 
+</template>
+<script>
+
+import Head from './head.vue'
+export default {
+components:{
+    'app-weather': Head
+}
+    
+}
+</script>
